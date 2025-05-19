cb75da2ffdd06af1442a4ae0dc85a4e7b5020eb2
sarikap9 <sarika@platform9.com>
2025-05-19 16:25:50 +0530

    deploy

--- src/components/PanelStatusTable/PanelStatusTable.js
+++ src/components/PanelStatusTable/PanelStatusTable.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Button, Checkbox, Icon, Table} from 'semantic-ui-react';
+import {Button, Checkbox, Table} from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import './PanelStatusTable.css';
 
